8e162b2cece0a782764e6d0888f9d48f8ccbd469
sahilsingh21 <ss3842486@gmail.com>
2025-06-18 11:57:18 +0530

    Without appling SOLID principle

---
+++ 17_interfaces/interface.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+//Payment gateway
+
+type payment struct {
+}
+
+func (p payment) makePayment(amount float32) {
+	// razorpayPaymentGw := razorpay{}
+	// razorpayPaymentGw.pay(amount)
+	stripepayPaymentGw := stripe{}
+	stripepayPaymentGw.pay(amount)
+
+}
+
+type razorpay struct{}
+
+func (r razorpay) pay(amount float32) {
+	// logic to make payment
+	fmt.Println("making payment using razorpay", amount)
+}
+
+type stripe struct{}
+
+func (s stripe) pay(amount float32) {
+	// logic to make payment
+	fmt.Println("making payment using stripe", amount)
+}
+
+func main() {
+	newPayment := payment{}
+	newPayment.makePayment(100)
+}
